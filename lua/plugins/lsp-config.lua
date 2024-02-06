03a4b99737592149307b36928f3c8a77fb0b1622
Jess <jessdaniel474@gmail.com>
2024-02-05 21:38:26 -0500

    adds autocomplete and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,46 +1,54 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-    require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "ansiblels",
-          "astro",
-          "bashls",
-          "cssls",
-          "dockerls",
-          "docker_compose_language_service",
-          "html",
-          "jsonls",
-          "tsserver",
-          "lua_ls",
-          "marksman",
-          "intelephense",
-          "jedi_language_server",
-          "rust_analyzer",
-          "sqlls",
-          "tailwindcss",
-          "terraformls",
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.rust_analyzer.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"ansiblels",
+					"astro",
+					"bashls",
+					"cssls",
+					"dockerls",
+					"docker_compose_language_service",
+					"html",
+					"jsonls",
+					"tsserver",
+					"lua_ls",
+					"marksman",
+					"intelephense",
+					"jedi_language_server",
+					"rust_analyzer",
+					"sqlls",
+					"tailwindcss",
+					"terraformls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
